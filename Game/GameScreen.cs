dd328c0e3e39b51f5f5a8f79f7796c46d0c81a30
Tomas Baltusis <tomas.baltusis@servelec.co.uk>
2020-04-25 17:07:24 +0100

    cleaned screen class

--- Game/GameScreen.cs
+++ Game/GameScreen.cs
@@ -9,79 +9,13 @@ namespace ConsoleGame.Game
 {
     class GameScreen
     {
-        private int _width;
-        private int _height;
-
-        private Hero _hero;
-        private List<Enemy> _enemies = new List<Enemy>();
+        private int width;
+        private int height;
 
         public GameScreen(int width, int height)
         {
-            _width = width;
-            _height = height;
+            this.width = width;
+            this.height = height;
         }
-
-        public void SetHero(Hero hero)
-        {
-            this._hero = hero;
-        }
-
-
-        public void MoveHeroLeft()
-        {
-            if (_hero.GetX() > 0)
-            {
-                _hero.MoveLeft();
-            }
-        }
-
-        public void MoveHeroRight()
-        {
-            if (_hero.GetX() < _width)
-            {
-                _hero.MoveRight();
-            }
-        }
-
-        public void AddEnemy(Enemy enemy)
-        {
-            _enemies.Add(enemy);
-        }
-
-        public void MoveAllEnemiesDown()
-        {
-            foreach (Enemy enemy in _enemies)
-            {
-                enemy.MoveDown();
-            }
-        }
-
-        public Enemy GetEnemyById(int id)
-        {
-            foreach (Enemy enemy in _enemies)
-            {
-                if (enemy.GetId() == id)
-                {
-                    return enemy;
-                }
-            }
-
-            return null;
-        }
-
-        public void Render()
-        {
-            _hero.PrintInfo();
-            foreach (Enemy enemy in _enemies)
-            {
-                enemy.PrintInfo();
-            }
-        }
-
-
-
-
-
-
     }
 }
